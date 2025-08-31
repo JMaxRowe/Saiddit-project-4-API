109c0facfd67832beea9b1b8d0130ad456b391f4
JMaxRowe <jmaxrowe@gmail.com>
2025-08-31 14:46:00 +0100

    completed post CRUD

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
 from .serializers.common import PostSerializer
+from rest_framework.exceptions import NotFound, PermissionDenied
 # Create your views here.
 
 class PostListView(APIView):
@@ -18,3 +19,35 @@ class PostListView(APIView):
         serialized_posts.is_valid(raise_exception=True)
         serialized_posts.save(poster=request.user)
         return Response(serialized_posts.data, 201)
+    
+class PostDetailView(APIView):
+
+    def get_post(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            return post
+        except Post.DoesNotExist as e:
+            print(e)
+            raise NotFound('Post not found.')
+
+    def get(self, request, pk):
+        post = self.get_post(pk)
+        serialized_post = PostSerializer(post)
+        return Response(serialized_post.data)
+    
+    def put(self, request, pk):
+        post = self.get_post(pk)
+        if post.poster != request.user:
+            raise PermissionDenied('You do not have permission to update this post.')
+        serialized_post = PostSerializer(post, data=request.data, partial=True)
+        serialized_post.is_valid(raise_exception=True)
+        serialized_post.save()
+        return Response('HIT UPDATE ROUTE')
+    
+    def delete(self, request, pk):
+        post = self.get_post(pk)
+        if post.poster != request.user:
+            raise PermissionDenied('You do not have permission to delete this post.')
+        post.delete()
+        return Response(status=204)
+    
