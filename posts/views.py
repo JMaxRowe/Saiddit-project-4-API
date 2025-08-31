839b723bd54df4b029b0049580f6a02213446b4a
JMaxRowe <jmaxrowe@gmail.com>
2025-08-31 14:01:24 +0100

    index route done

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers.common import PostSerializer
 # Create your views here.
+
+class PostListView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serialized_posts = PostSerializer(posts, many=True)
+        return Response(serialized_posts.data)
--- saiddit_project_4/urls.py
+++ saiddit_project_4/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('communities/', include('communities.urls'))
+    path('communities/', include('communities.urls')),
+    path('posts/', include('posts.urls')),
 ]
