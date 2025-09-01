2738baad72808f9e9479e4a29e1b48fa1c786180
JMaxRowe <jmaxrowe@gmail.com>
2025-09-01 15:21:49 +0100

    fixed mismatching client/api info

--- saiddit_project_4/settings.py
+++ saiddit_project_4/settings.py
@@ -28,9 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
     'posts',
     'comments',
     'votes',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -66,6 +69,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- saiddit_project_4/urls.py
+++ saiddit_project_4/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('auth/', include('users.urls')),
     path('communities/', include('communities.urls')),
     path('posts/', include('posts.urls')),
     path('comments/', include('comments.urls')),
